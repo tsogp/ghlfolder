6a51d40bbf16718976155e601d9e5d5e4977341c
Potemkin Pavel <potemkinpavel3@gmail.com>
2025-03-18 00:12:37 +0700

    progress_bar refactoring

--- include/progress_bar.hpp
+++ include/progress_bar.hpp
@@ -8,12 +8,24 @@
 #include <sys/ioctl.h>
 #include <unistd.h>
 
-template <int MIN_WIDTH = 10, int MAX_WIDTH = 100>
+static constexpr int BAR_POSTFIX_SIZE = 7;
+static constexpr int BAR_MIN_WIDTH = 10;
+static constexpr int BAR_MAX_WIDTH = 100;
+
+template <int MIN_WIDTH = BAR_MIN_WIDTH, int MAX_WIDTH = BAR_MAX_WIDTH>
 class progress_bar {
 public:
-    explicit progress_bar(int width, bool init = false) : width_(std::clamp(width, MIN_WIDTH, MAX_WIDTH)) {
-        if (init) {
-            init_bar();
+    progress_bar() = delete;
+
+    explicit progress_bar(int width) : width_(std::clamp(width, MIN_WIDTH, MAX_WIDTH)) {
+    }
+
+    void init_bar() {
+        if (!is_initiated) {
+            std::cout << "[" << std::setw(pos_) << std::setfill('#') << "";
+            std::cout << std::setw(width_ - pos_) << std::setfill(' ') << "";
+            std::cout << ']' << std::right << std::setw(5) << std::setfill(' ') << static_cast<int>(ceil(progress_ * 100.0)) << "%";
+            is_initiated = true;
         }
     }
 
@@ -28,7 +40,7 @@ public:
         int new_pos = static_cast<int>(ceil(width_ * progress_));
         int char_progress = new_pos - pos_;
 
-        std::cout << "\033[" << width_ - pos_ + BAR_POSTFIX_SIZE << "D";
+        std::cout << "\033[" << width_ - pos_ - 1 + BAR_POSTFIX_SIZE << "D";
         std::cout << std::setw(char_progress) << std::setfill('#') << "";
         std::cout << "\033[" << (width_ - new_pos + 1) << "C";
         std::cout << std::setw(5) << std::setfill(' ') << static_cast<int>(ceil(progress_ * 100.0)) << "%";
@@ -40,7 +52,7 @@ public:
     void resize(int new_width) {
         is_initiated = false;
         width_ = std::clamp(new_width, MIN_WIDTH, MAX_WIDTH);
-        pos_ = static_cast<int>(ceil(width_ * progress_));\
+        pos_ = static_cast<int>(ceil(width_ * progress_));
         init_bar();
     }
 
@@ -49,17 +61,6 @@ public:
     }
 
 private:
-    void init_bar() {
-        if (!is_initiated) {
-            std::cout << "[" << std::setw(pos_) << std::setfill('#') << "";  
-            std::cout << std::setw(width_ - pos_) << std::setfill(' ') << "";
-            std::cout << ']' << std::setw(5) << std::setfill(' ') << static_cast<int>(ceil(progress_ * 100.0)) << "%";
-            is_initiated = true;
-        }
-    }
-
-    static constexpr int BAR_POSTFIX_SIZE = 7;
-
     int width_, pos_{};
     double progress_{};
     bool is_initiated{};
