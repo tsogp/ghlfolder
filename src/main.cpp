b998ac6a10e33d01a815f2bd4d0d1995d225b6dd
Potemkin Pavel <potemkinpavel3@gmail.com>
2025-03-18 23:18:43 +0700

    main.cpp created

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,39 @@
-#include <curl/curl.h>
+#include "curl_wrapper.hpp"
+#include <cmath>
+#include <filesystem>
 #include <iostream>
-#include <memory>
+#include <regex>
+#include <sys/ioctl.h>
+#include <unistd.h>
 
-int main(int argc, char **argv) {
-    CURL *handler = curl_easy_init();
+std::string convert_github_url(const std::string &url) {
+    std::regex pattern(R"(https://github\.com/([^/]+)/([^/]+)/tree/([^/]+)/(.*))");
+    std::smatch match;
 
-    curl_easy_setopt(handler, CURLOPT_URL, "http://example.com");
-    curl_easy_setopt(handler, CURLOPT_NOPROGRESS, 1L);
-    curl_easy_setopt(handler, CURLOPT_USERAGENT, "curl/7.45.0");
-    curl_easy_setopt(handler, CURLOPT_MAXREDIRS, 50L);
+    if (std::regex_match(url, match, pattern) && match.size() == 5) {
+        std::string author = match[1];
+        std::string repo = match[2];
+        std::string branch = match[3];
+        std::string folder = match[4];
 
-    CURLcode ret = curl_easy_perform(handler);
+        std::cout << "Cloning into " << folder << "...\n";
+        std::filesystem::create_directory(folder);
 
-    curl_easy_cleanup(handler);
+        return "https://api.github.com/repos/" + author + "/" + repo + "/contents/" + folder + "?ref=" + branch;
+    } 
 
-    return static_cast<int>(ret);
+    return "Invalid GitHub tree URL";
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <GitHub tree URL>\n";
+        return 1;
+    }
+
+    std::string api_url = convert_github_url(argv[1]);
+
+    curl_wrapper downloader(api_url);
+    downloader.download_files();
+    return 0;
 }
