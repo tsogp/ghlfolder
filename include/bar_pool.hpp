23548b54e9b8148f59a6d62d348899e90a1609ce
Potemkin Pavel <potemkinpavel3@gmail.com>
2025-06-02 15:47:30 +0300

    move bar_pool impl into object file

--- include/bar_pool.hpp
+++ include/bar_pool.hpp
@@ -2,7 +2,6 @@
 #define __BAR_POOL_HPP__
 
 #include "fetch_bar.hpp"
-#include <algorithm>
 #include <cstddef>
 #include <memory>
 #include <mutex>
@@ -16,53 +15,15 @@ public:
     bar_pool &operator=(bar_pool &&bp) = delete;
 
     template <typename... Bars>
-    explicit bar_pool(Bars &&...bars) {
-        (bars_.emplace_back(std::move(bars)), ...);
-        total_bars_ = bars_.size();
-        std::for_each(bars_.begin(), bars_.end(), [](const auto &bar) { bar->display(); });
-    }
-
-    std::size_t push_back(std::unique_ptr<fetch_bar> bar) {
-        std::scoped_lock lck(mutex_);
-        bar->display();
-        bars_.push_back(std::move(bar));
-        return total_bars_++;
-    }
-
-    void tick_i(std::size_t index, double progress) {
-        std::scoped_lock lck(mutex_);
-        std::size_t offset = total_bars_ - 1 - index;
-        move_cursor_up(offset);
-        bars_[index]->tick(progress);
-        move_cursor_down(offset);
-    }
-
-    bool is_i_complete(std::size_t index) {
-        std::scoped_lock lck(mutex_);
-        return bars_[index]->is_complete();
-    }
-
+    explicit bar_pool(Bars &&...bars);
+    std::size_t push_back(std::unique_ptr<fetch_bar> bar);
+    void tick_i(std::size_t index, double progress);
+    bool is_i_complete(std::size_t index);
 private:
     std::mutex mutex_;
     std::vector<std::unique_ptr<fetch_bar>> bars_;
     std::size_t total_bars_{};
     bool started_{};
-
-    static void move_cursor_up(std::size_t l) {
-        if (l != 0) {
-            // TODO: think of a way to remove this offset
-            std::cout << "\033[" << 1 << "C";
-            std::cout << "\033[" << l << "A";
-        }
-    }
-
-    static void move_cursor_down(std::size_t l) {
-        if (l != 0) {
-            // TODO: think of a way to remove this offset
-            std::cout << "\033[" << 1 << "C";
-            std::cout << "\033[" << l << "B";
-        }
-    }
 };
 
 #endif // __BAR_POOL_HPP__
---
+++ src/bar_pool.cpp
@@ -0,0 +1,54 @@
+#include "bar_pool.hpp"
+#include "fetch_bar.hpp"
+#include <algorithm>
+#include <cstddef>
+#include <memory>
+#include <mutex>
+#include <vector>
+
+namespace {
+    void move_cursor_up(std::size_t l) {
+        if (l != 0) {
+            // TODO: think of a way to remove this offset
+            std::cout << "\033[" << 1 << "C";
+            std::cout << "\033[" << l << "A";
+        }
+    }
+
+    void move_cursor_down(std::size_t l) {
+        if (l != 0) {
+            // TODO: think of a way to remove this offset
+            std::cout << "\033[" << 1 << "C";
+            std::cout << "\033[" << l << "B";
+        }
+    }
+};
+
+template bar_pool::bar_pool();
+
+template <typename... Bars>
+bar_pool::bar_pool(Bars &&...bars) {
+    (bars_.emplace_back(std::move(bars)), ...);
+    total_bars_ = bars_.size();
+    std::for_each(bars_.begin(), bars_.end(), [](const auto &bar) { bar->display(); });
+}
+
+std::size_t bar_pool::push_back(std::unique_ptr<fetch_bar> bar) {
+    std::scoped_lock lck(mutex_);
+    bar->display();
+    bars_.push_back(std::move(bar));
+    return total_bars_++;
+}
+
+void bar_pool::tick_i(std::size_t index, double progress) {
+    std::scoped_lock lck(mutex_);
+    std::size_t offset = total_bars_ - 1 - index;
+    move_cursor_up(offset);
+    bars_[index]->tick(progress);
+    move_cursor_down(offset);
+}
+
+bool bar_pool::is_i_complete(std::size_t index) {
+    std::scoped_lock lck(mutex_);
+    return bars_[index]->is_complete();
+}
