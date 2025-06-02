0176553f6bcab80fc07fc78115d26f696d387ea3
Potemkin Pavel <potemkinpavel3@gmail.com>
2025-06-02 15:05:04 +0300

    move matcher utils into an object file

--- include/matcher.hpp
+++ include/matcher.hpp
@@ -7,28 +7,7 @@
 #include <string_view>
 
 namespace matcher {
-
-namespace {
-using namespace ctre::literals;
-
-constexpr auto github_pattern = R"((?:https?:\/\/)?github\.com/([^/]+)/([^/]+)/tree/([^/]+)/(.*))"_ctre;
-constexpr auto gitlab_pattern =
-    R"((?:https?:\/\/)?gitlab\.com/([^\/]+)/([^\/]+)/-/tree/([^\/]+)/([^?]+)(?:\?ref_type=heads)?)"_ctre;
-}
-
-std::unique_ptr<r_base> get_repo_data(std::string_view url, bool create_dir = false) {
-    if (auto [whole, author, repo, branch, folder] = github_pattern.match(url); whole) {
-        return std::make_unique<r_github>(author.to_view(), repo.to_view(), branch.to_view(), folder.to_view(), create_dir);
-    }
-
-    // TODO: implement GitLab 
-    // if (auto [whole, author, repo, branch, folder] = gitlab_pattern.match(url); whole) {
-    //     return std::make_unique<r_gitlab>(author.to_view(), repo.to_view(), branch.to_view(), folder.to_view(), write_dir);
-    // }
-
-    return nullptr;
-}
-
+std::unique_ptr<r_base> get_repo_data(std::string_view url, bool create_dir = false);
 } // namespace matcher
 
 #endif // __MATCHER_HPP__
---
+++ src/matcher.cpp
@@ -0,0 +1,29 @@
+#include "repo.hpp"
+#include <ctre.hpp>
+#include <memory>
+#include <string_view>
+
+namespace {
+using namespace ctre::literals;
+
+constexpr auto github_pattern = R"((?:https?:\/\/)?github\.com/([^/]+)/([^/]+)/tree/([^/]+)/(.*))"_ctre;
+constexpr auto gitlab_pattern =
+    R"((?:https?:\/\/)?gitlab\.com/([^\/]+)/([^\/]+)/-/tree/([^\/]+)/([^?]+)(?:\?ref_type=heads)?)"_ctre;
+}
+
+namespace matcher {
+
+std::unique_ptr<r_base> get_repo_data(std::string_view url, bool create_dir = false) {
+    if (auto [whole, author, repo, branch, folder] = github_pattern.match(url); whole) {
+        return std::make_unique<r_github>(author.to_view(), repo.to_view(), branch.to_view(), folder.to_view(), create_dir);
+    }
+
+    // TODO: implement GitLab 
+    // if (auto [whole, author, repo, branch, folder] = gitlab_pattern.match(url); whole) {
+    //     return std::make_unique<r_gitlab>(author.to_view(), repo.to_view(), branch.to_view(), folder.to_view(), write_dir);
+    // }
+
+    return nullptr;
+}
+
+} // namespace matcher
